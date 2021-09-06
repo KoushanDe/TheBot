2fe3770be63594f2d127e53e6f5408c050cf86d3
Koushan007 <koushanriki007@gmail.com>
2021-09-07 02:02:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,12 +48,7 @@ class MusicBot(commands.Bot):
     async def change_status(self):
         # sets status of the bot
         await self.change_presence(activity=discord.Game(choice(status)))
-
-    # async def on_error(self, err, *args, **kwargs):
-    #     raise
-
-    # async def on_command_error(self, ctx, exc):
-    #     raise getattr(exc, "original", exc)
+        
 
     async def on_ready(self):
         self.client_id = (await self.application_info()).id
